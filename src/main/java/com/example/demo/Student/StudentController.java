58ad0485f0a288911beb8e4ab1a8835ae6c6702c
Felipe Sánchez Soberanis <fs@wayakna.com>
2021-12-28 14:58:41 -0600

    Local list of students with spring security

--- src/main/java/com/example/demo/Student/StudentController.java
+++ src/main/java/com/example/demo/Student/StudentController.java
@@ -1,5 +1,8 @@
 package com.example.demo.Student;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +12,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/v1/students")
 public class StudentController {
 
-    @GetMapping(path = "{studentId}")
-    public Student getStudent(@PathVariable("studentId") int studentId) {
+    private static final List<Student> STUDENTS = Arrays.asList(
+        new Student(1, "James Bond"),
+        new Student(2, "Maria Jones"),
+        new Student(3, "Anna Smith")
+    );
 
+    @GetMapping(path = "{studentId}")
+    public Student getStudent(@PathVariable("studentId") Integer studentId) {
+        return STUDENTS.stream()
+            .filter(student -> studentId.equals(student.getStudentId()))
+            .findFirst().orElseThrow(() -> new IllegalStateException("Student not found"));
     }
 
 }
