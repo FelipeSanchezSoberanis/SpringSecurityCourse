481065df61813020ee85d16ef52e3d4e1b4922b2
Felipe Sánchez Soberanis <fs@wayakna.com>
2021-12-29 13:40:02 -0600

    GSON

--- src/main/java/com/example/demo/Student/Student.java
+++ src/main/java/com/example/demo/Student/Student.java
@@ -1,17 +1,22 @@
 package com.example.demo.Student;
 
+import com.google.gson.Gson;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter @Setter
 @AllArgsConstructor
-@ToString
 public class Student {
 
     private final Integer studentId;
     private final String studentName;
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
 
 }
 
