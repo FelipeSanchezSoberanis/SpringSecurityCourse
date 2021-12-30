d0b41c00fa92ba5f04a27eb2e682559a6f25a29a
Felipe Sánchez Soberanis <fs@wayakna.com>
2021-12-30 11:30:49 -0600

    preAuthorize

--- src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.example.demo.Security;
 
+import static com.example.demo.Security.ApplicationUserRole.ADMIN;
+import static com.example.demo.Security.ApplicationUserRole.ADMINTRAINEE;
+import static com.example.demo.Security.ApplicationUserRole.STUDENT;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -13,11 +17,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
-import static com.example.demo.Security.ApplicationUserRole.*;
-import static com.example.demo.Security.ApplicationUserPermission.*;
-
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PasswordEncoder passwordEncoder;
@@ -34,10 +36,6 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
             .authorizeRequests()
             .antMatchers("/", "index", "/css/*", "/js/*").permitAll()
             .antMatchers("/api/**").hasAuthority(STUDENT.getRole())
-            .antMatchers(HttpMethod.DELETE, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-            .antMatchers(HttpMethod.POST, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-            .antMatchers(HttpMethod.PUT, "/management/api/**").hasAuthority(COURSE_WRITE.getPermission())
-            .antMatchers(HttpMethod.GET, "/management/api/**").hasAnyAuthority(ADMIN.getRole(), ADMINTRAINEE.getRole())
             .anyRequest()
             .authenticated()
         .and()
