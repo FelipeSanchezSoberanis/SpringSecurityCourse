c071709d151c9193bf3f1bd7d0b215f28b0de833
Felipe Sánchez Soberanis <fs@wayakna.com>
2021-12-30 13:23:24 -0600

    Logout

--- src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/Security/ApplicationSecurityConfig.java
@@ -47,7 +47,14 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         .and()
             .rememberMe()
             .tokenValiditySeconds((int)TimeUnit.DAYS.toSeconds(21))
-            .key("somethingVerySecure");
+            .key("somethingVerySecure")
+        .and()
+            .logout()
+            .logoutUrl("/logout")
+            .clearAuthentication(true)
+            .invalidateHttpSession(true)
+            .deleteCookies("JSESSIONID", "remember-me")
+            .logoutSuccessUrl("/login");
     }
 
     @Override
