227c6ba3dc81a3452904012258c074b44a705553
Felipe Sánchez Soberanis <fs@wayakna.com>
2022-01-02 16:15:35 -0600

    JwtUsernameAndPasswordAuthenticationFilter successfulAuthentication

--- src/main/java/com/example/demo/Jwt/JwtUsernameAndPasswordAuthenticationFilter.java
+++ src/main/java/com/example/demo/Jwt/JwtUsernameAndPasswordAuthenticationFilter.java
@@ -1,7 +1,11 @@
 package com.example.demo.Jwt;
 
 import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Date;
 
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
@@ -14,6 +18,9 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 
 public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
 
@@ -31,8 +38,7 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
                     .readValue(request.getInputStream(), UsernameAndPasswordAuthenticationRequest.class);
 
             Authentication authentication = new UsernamePasswordAuthenticationToken(
-                    authenticationRequest.getUsername(), authenticationRequest.getPassword()
-            );
+                    authenticationRequest.getUsername(), authenticationRequest.getPassword());
 
             return authenticationManager.authenticate(authentication);
         } catch (StreamReadException e) {
@@ -43,6 +49,21 @@ public class JwtUsernameAndPasswordAuthenticationFilter extends UsernamePassword
             e.printStackTrace();
         }
         return super.attemptAuthentication(request, response);
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain,
+            Authentication authResult) throws IOException, ServletException {
+
+        String token = Jwts.builder()
+                .setSubject(authResult.getName())
+                .claim("authorities", authResult.getAuthorities())
+                .setIssuedAt(new Date())
+                .setExpiration(java.sql.Date.valueOf(LocalDate.now().plusWeeks(2)))
+                .signWith(Keys.hmacShaKeyFor("securesecuresecuresecuresecure".getBytes()))
+                .compact();
+
+        response.addHeader("Authorization", "Bearer " + token);
     }
 
 }
